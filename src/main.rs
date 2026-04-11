6223c6452122a3460a81df4e73985d0279f50160
James Vongphasouk <james.vongpha@gmail.com>
2026-04-11 12:18:54 -0600

    feat: ingest pokemon struct

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,53 @@
 use ureq::get;
 use std::error::Error;
 use serde::{Serialize, Deserialize};
+use serde_json::Value;
 
+// only use the cries and sprites url to render later
 #[derive(Serialize, Deserialize, Debug)]
-struct Task {
-    userId: i32,
-    id: i32,
-    title: String,
-    completed: bool
+#[serde(rename_all = "camelCase")]
+struct Pokemon { 
+    pub cries: Cries,
+    pub sprites: Sprites,
 }
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Sprites {
+    #[serde(rename = "back_default")]
+    pub back_default: String,
+    #[serde(rename = "back_female")]
+    pub back_female: Value,
+    #[serde(rename = "back_shiny")]
+    pub back_shiny: String,
+    #[serde(rename = "back_shiny_female")]
+    pub back_shiny_female: Value,
+    #[serde(rename = "front_default")]
+    pub front_default: String,
+    #[serde(rename = "front_female")]
+    pub front_female: Value,
+    #[serde(rename = "front_shiny")]
+    pub front_shiny: String,
+    #[serde(rename = "front_shiny_female")]
+    pub front_shiny_female: Value,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Cries {
+    pub latest: String,
+    pub legacy: String,
+}
+
 // main usually returns () and Box<dyn Error> is a trait object (short for any type of error)
 fn main() -> Result<(), Box< dyn Error>> { 
     // works!!!
-    //"https://pokeapi.co/api/v2/pokemon/pikachu"
-
-    let uri = "https://jsonplaceholder.typicode.com/todos/10"; 
+    let uri = "https://pokeapi.co/api/v2/pokemon/pikachu"; 
     let body: String = get(uri)
         .call()?
         .body_mut()
         .read_to_string()?;
 
-    let task: Task = serde_json::from_str(&body)?;
-
-    dbg!(task.userId);
-    dbg!(task.id);
-    dbg!(task.title);
-    dbg!(task.completed);
-    dbg!(task.userId);
+    let pokemon : Pokemon = serde_json::from_str(&body)?;
+    dbg!(pokemon);
     Ok(())
-    // need to use Serde to do things with json data... we'll be right back..
 }
